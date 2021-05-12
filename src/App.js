1dea6b8e883dbb6e21943b725c1f987be4655407
Cheli Kalina <cheli@frontegg.com>
2021-05-12 12:03:07 +0300

    first commit - login ui html/css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./leaf.png";
+import loginVisual from "./img.png";
+import "./index.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex-row">
+      <div className="flex-center">
+          <img src={logo} alt="logo" className="logo" />
+          <h1>WELCOME</h1>
+          <input placeholder="Username" />
+          <input placeholder="Password" />
+          <a href='' >Forgot Password?</a>
+          <button className="submit">Submit</button>
+          <hr/>
+          <div className="center">
+            <div>
+          <button>Google</button>
+          <button>Github</button>
+            </div>
+            <div>
+          <button>Facebook</button>
+          <button>Microsoft</button>
+            </div>
+          <hr/>
+          <a href='' >Create an Account</a>
+          </div>
+        </div>
+        <div className="bg">
+          <img src={loginVisual} alt="img" className="visual" />
+        </div>
+      </div>
+    </>
   );
 }
 
